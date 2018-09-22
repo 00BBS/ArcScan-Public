ccc8864640f0b37b6287f869d19a57644414107a
Khye Ean <lowkhyeean@gmail.com>
2018-09-22 10:51:38 +1000

    Implemented database models

--- routes.py
+++ routes.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, render_template, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///databases/main.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+db = SQLAlchemy(app)
 
 @app.route('/', methods = ['GET'])
 def index():
