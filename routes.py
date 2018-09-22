48a275c272bc7b4b0d63a01a74317caba6287417
Khye Ean <lowkhyeean@gmail.com>
2018-09-22 11:26:27 +1000

    Added register queries

--- routes.py
+++ routes.py
@@ -8,8 +8,23 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 #this would be login
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        new_user = ('zid' = request.form['zid'],
+                    'name' = request.form['name'],
+                    'arc' = request.form['arc'],
+                    'password' = request.form['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return "Registered"
+        '''
+        # find user
+        user_check = Users.query.filter_by(zid=request.form['zid']).first()
+        if user_check:
+            if user_check.password == request.form['password']:
+                return "registered"
+        '''
     return render_template('index.html')
 
 @app.route('/profile/<user>', methods = ['GET']
