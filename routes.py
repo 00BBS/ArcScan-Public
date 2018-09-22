8e6b9b620412b7c68235e63124fe70306d79dd96
Khye Ean <lowkhyeean@gmail.com>
2018-09-22 14:49:22 +1000

    Added Flask-Login and implemented create events page

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from random_key import *
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases/main.db'
@@ -8,7 +9,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Societies, Events, Users
-#this would be login
+
+## Flask-Login configs ##
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='/'
+#########################
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     return render_template('index.html')
@@ -20,7 +27,7 @@ def index():
         else:
                 return render_template('profInd.html')
 '''
-@app.route('/soclogin')
+@app.route('/soclogin', methods=['GET','POST'])
 def soclogin():
     if request.method == "POST":
         username_check = request.form['username']
@@ -28,21 +35,23 @@ def soclogin():
         # query based on username
         society = Societies.query.filter_by(username = username_check).first()
         if (password == society.password):
+            login_user(society)
             return "Logged in, welcome back " + society.name
     return render_template('soclogin.html')
 
-@app.route('/stulogin')
+@app.route('/stulogin', methods=['GET','POST'])
 def stulogin():
     if request.method == "POST":
         zid_check = request.form['zid']
         password_check = request.form['password']
         # query based on username
-        user = Users.query.filter_by(zid = zid_checl).first()
+        user = Users.query.filter_by(zid = zid_check).first()
         if (password == user.password):
+            login_user(user)
             return "Logged in, welcome back " + user.name
     return render_template('stulogin.html')
 
-@app.route('/sturegister')
+@app.route('/sturegister', methods=['GET','POST']  )
 def sturegister():
     if request.method == "POST":
         new_user = Users(zid = request.form['zid'],
@@ -55,7 +64,7 @@ def sturegister():
         return "Thanks for registering, " + new_user.name
     return render_template('sturegister.html')
 
-@app.route('/socregister')
+@app.route('/socregister', methods = ['GET', 'POST'])
 def socregister():
     if request.method == "POST":
         new_soc = Societies(name = request.form['name'],
@@ -66,4 +75,39 @@ def socregister():
         ### TODO: REDIRECT TO LOGIN DASHBOARD
         return "Thanks for registering, " + new_soc.name
     return socregister('socregister.html')
+
+@app.route('/event/<int:event_id>', methods = ['GET', 'POST'])
+def event(event_id):
+    event = Events.query.filter_by(id = event_id).one()
+
+    # handles POST method (play button to generate 6 digit code)
+    if request.method == "POST":
+        pushing_code = Events(secret_code = verify_key())
+        db.session.add(pushing_code)
+        db.session.commit()
+
+    # returns event specific page
+    return render_template('event.html', event = event) 
+
+@app.route('/create', methods = ['GET','POST'])
+def create:
+    new_event = Events(name = request.form['name'],
+                location = request.form['location'],
+                starttime = request.form['starttime'],
+                date = request.form['date'],
+                secret_code = "000000",
+                society =  current_user.name)
+    db.session.add(new_event)
+    db.session.commit()
+    return render_template('create.html')
+
+
+
+
+
+
+
+
+
+
 
